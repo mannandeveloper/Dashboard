5659f54aaf414eca3583fffec2e0efcdc4f9e626
mannandeveloper <mannandeveloper@gmail.com>
2019-09-28 02:54:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dashboard/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
